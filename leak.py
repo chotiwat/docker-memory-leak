fdaba054e5e6963037d019e4468a22f8b7eb1fe4
Chotiwat Chawannakul <chotiwat.c@hotmail.com>
2019-06-06 14:59:26 -0700

    Update leak.py

--- leak.py
+++ leak.py
@@ -1,8 +1,12 @@
-import random
+import sys
+import time
 
 def leak():
-    dataList = [];
+    dataList = []
+    interval = float(sys.argv[1]) if len(sys.argv) > 1 else None
     while True:
-        dataList.append(1337L);
+        dataList.append(1337L)
+        if interval is not None:
+                time.sleep(interval)
 
 leak()
