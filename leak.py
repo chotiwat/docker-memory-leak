698b928bee42fc554dc9213efe66953b08bd0b5d
Chotiwat Chawannakul <chotiwat@blend.com>
2019-06-06 17:00:21 -0700

    Leak faster

--- leak.py
+++ leak.py
@@ -5,7 +5,8 @@ def leak():
     dataList = []
     interval = float(sys.argv[1]) if len(sys.argv) > 1 else None
     while True:
-        dataList.append(1337L)
+	for i in range(1 << 20):
+        	dataList.append(1337L)
         if interval is not None:
                 time.sleep(interval)
 
